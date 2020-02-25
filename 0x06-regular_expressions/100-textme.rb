08c9d401f0c778734884c5d81b9dcf01825d75e6
danielcinome <danielchinome987@gmail.com>
2020-02-25 02:06:29 +0000

    [Task Advanced 100] Correction flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 a = ARGV[0].scan(/from:(\w+|.\d+)/).join
-b = ARGV[0].scan(/to:(.\d+)/).join
-c = ARGV[0].scan(/flags:(.\d+:(\d+|.\d+):.\d+:(\d+|.\d+):.\d+)/).join
+b = ARGV[0].scan(/to:(\w+|.\d+)/).join
+c = ARGV[0].scan(/flags:([0-9.+-:]+)/).join
 puts("#{a},#{b},#{c}")
