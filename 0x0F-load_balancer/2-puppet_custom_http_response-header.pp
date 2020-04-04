de1642d72a1d3f8c48a319217a128cb25c76bbec
danielcinome <danielchinome987@gmail.com>
2020-04-04 05:02:58 +0000

    [Task 2]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,5 @@
 # Add a custom HTTP header with Puppet
 exec { 'Puppet HTTP':
-command => "apt-get update && apt-get -y install nginx"
-command => "echo -e 'Holberton School' > /var/www/html/index.html"
-command => "sed -i '/sendfile on;/a add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
+command => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '/sendfile on;/a add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
 provider => 'shell',
 }
