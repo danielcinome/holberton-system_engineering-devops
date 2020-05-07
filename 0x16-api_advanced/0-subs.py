96628e5beee77b012d8c95ffd30cd3bd30adafcf
danielcinome <danielchinome987@gmail.com>
2020-05-07 17:14:43 +0000

    Task Complete

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,14 @@
 """ Request Reddit API """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ numbers of subs """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     tok = {'user-agent': 'X-Modhash'}
     req = requests.get(url, headers=tok)
     if req.history or req.status_code == 404:
-       return 0
+        return 0
     else:
-       req = req.json()
-       return req.get('data').get('subscribers')
+        req = req.json()
+        return req.get('data').get('subscribers')
