7f3c4fee000224af78de50d236e1cd1354115846
danielcinome <danielchinome987@gmail.com>
2020-05-20 20:14:25 +0000

    change command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #repair apache error 500
-exec { 'error':
+exec { 'repair error':
+  provider => shell,
   command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );+require_once\
 ( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
 }
