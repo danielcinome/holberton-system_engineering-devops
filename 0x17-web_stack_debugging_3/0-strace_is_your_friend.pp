2fec404c0cd97d7cb20202acc2c6a0028f0cf3a2
danielcinome <danielchinome987@gmail.com>
2020-05-20 19:46:10 +0000

    Correction deleted \n

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # repair apache error 500
 exec { 'error':
-  command => 'sed 's/class-wp-locale.phpp/class-wp-locale.php/'\
-  /var/www/html/wp-settings.php'
+  command => 'sed 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php'
 }
