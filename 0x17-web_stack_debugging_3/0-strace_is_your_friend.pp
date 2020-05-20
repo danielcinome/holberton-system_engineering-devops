7979c43814d1303c2e2b6d79326dac128e6c24f3
danielcinome <danielchinome987@gmail.com>
2020-05-20 19:50:03 +0000

    deleted provider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 i repair apache error 500
 exec { 'error':
-  provider => shell,
-  command => 'sed 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php'
+  command => 'sed 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php',
 }
