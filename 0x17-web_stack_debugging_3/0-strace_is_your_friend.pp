3b740fde270f4d1c4f4343a4584069604b598dcb
danielcinome <danielchinome987@gmail.com>
2020-05-20 19:48:56 +0000

    add provider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# repair apache error 500
+i repair apache error 500
 exec { 'error':
+  provider => shell,
   command => 'sed 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php'
 }
