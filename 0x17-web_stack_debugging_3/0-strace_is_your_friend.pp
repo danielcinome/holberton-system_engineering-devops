900eaa70445cc55f69550bc45765bf9f550340fc
danielcinome <danielchinome987@gmail.com>
2020-05-20 20:13:02 +0000

    change command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-i repair apache error 500
+#repair apache error 500
 exec { 'error':
-  command => 'sed -i "s+require_once( ABSPATH . WPINC . \
-  '/class-wp-locale.phpp\' );+require_once( ABSPATH . WPINC . \
-  '/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );+require_once\
+( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" /var/www/html/wp-settings.php',
 }
